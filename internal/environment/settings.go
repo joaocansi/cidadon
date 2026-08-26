1f22b001e1210f6c6134deab52740821204cd5a6
joaocansi <whitshs2003@gmail.com>
2026-08-26 00:47:46 -0300

    Feat: define application settings

--- internal/environment/settings.go
+++ internal/environment/settings.go
@@ -1,6 +1,11 @@
 package environment
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -44,4 +49,47 @@ func Load() error {
 		return err
 	}
 	return nil
+}
+
+type RSA256Pair struct {
+	PublicKey  *rsa.PublicKey
+	PrivateKey *rsa.PrivateKey
+}
+
+func LoadRsaKeys(privatePath, publicPath string) (*RSA256Pair, error) {
+	privateBytes, err := os.ReadFile(privatePath)
+	if err != nil {
+		return nil, err
+	}
+
+	privateBlock, _ := pem.Decode(privateBytes)
+
+	key, err := x509.ParsePKCS8PrivateKey(privateBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	privateKey, ok := key.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("not an RSA private key")
+	}
+
+	publicBytes, err := os.ReadFile(publicPath)
+	if err != nil {
+		return nil, err
+	}
+
+	publicBlock, _ := pem.Decode(publicBytes)
+
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(publicBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKey := publicKeyInterface.(*rsa.PublicKey)
+
+	return &RSA256Pair{
+		PublicKey:  publicKey,
+		PrivateKey: privateKey,
+	}, nil
 }
